f54b4bdd48abc83d0eaf3f7747ade4dcbb51cb37
Andrew Preece <andrew.preece@gmail.com>
2026-03-23 13:22:25 +0000

    fix: remove create_workspace/delegate_workspace refs not yet in stack

--- src/tools/builtin/mod.rs
+++ src/tools/builtin/mod.rs
@@ -1,8 +1,6 @@
 //! Built-in tools that come with the agent.
 
 mod conversation;
-mod create_workspace;
-mod delegate_workspace;
 mod discover_tools;
 mod echo;
 pub mod extension_tools;
@@ -22,8 +20,6 @@ mod time;
 mod tool_info;
 
 pub use conversation::ConversationLoadTool;
-pub use create_workspace::CreateWorkspaceTool;
-pub use delegate_workspace::DelegateToWorkspaceTool;
 pub use discover_tools::DiscoverToolsTool;
 pub use echo::EchoTool;
 pub use extension_tools::{
